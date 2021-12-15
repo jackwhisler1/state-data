6906ed3b0ecf9b9402eaf55a088c044b68274791
jack <whisler1@gmail.com>
2021-12-15 16:37:29 -0600

    adds state model

---
+++ db/migrate/20211215223504_create_states.rb
@@ -0,0 +1,13 @@
+class CreateStates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :states do |t|
+      t.string :name
+      t.integer :median_household_income
+      t.decimal :share_unemployed_seasonal, precision: 4, scale: 3
+      t.decimal :share_population_in_metro_areas, precision: 3, scale: 2
+      t.decimal :share_population_with_high_school_degree, precision: 4, scale: 3
+
+      t.timestamps
+    end
+  end
+end
